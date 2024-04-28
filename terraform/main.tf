24278773020fcdc3db8a3ba6ea4efb921138e95e
Eduardo Dias <edu_rodriguesdias@hotmail.com>
2024-04-28 13:54:14 -0300

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,14 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws",
-      version = "~> 5.0"
+      version = "~>3.27"
     }
   }
+
+  required_version = ">=0.14.9"
 }
 
 provider "aws" {
-  region = "us-east-1"
+  version = "~>3.0"
+  region  = "east-us-1"
 }
